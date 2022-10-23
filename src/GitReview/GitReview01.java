5143dc086e5009b7e27f303b90dbde3b8a64d545
melikedalgin <mlkedalgin@gmail.com>
2022-10-22 23:09:45 -0400

    first commit

---
+++ src/GitReview/GitReview01.java
@@ -0,0 +1,11 @@
+package GitReview;
+
+public class GitReview01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("ne yazsak ki");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author melikedalgin
+ *
+ */
+module GitReview {
+}
